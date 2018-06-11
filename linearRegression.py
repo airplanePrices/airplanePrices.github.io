03ed2007a0f491ddee8657e94bdafdc8bdd494ca
Ethan Lee <ethanlee2020@u.northwestern.edu>
2018-06-10 23:41:21 -0500

    trying  model on other stuff

--- linearRegression.py
+++ linearRegression.py
@@ -1,12 +1,15 @@
 import pandas as pd
+import numpy as np
 import csv
 import ipdb
 import matplotlib.pyplot as plt
 from sklearn.linear_model import LinearRegression
+from sklearn.model_selection import train_test_split
+import sklearn
 
 def MakeLinearRegressionModel(filename):
   dataset = pd.read_csv(filename, error_bad_lines=False)
-  dataset = dataset[:46669]
+  dataset = dataset[:42000]
   dataset = dataset.apply(pd.to_numeric, errors='ignore')
 
   X = dataset.drop('cost', axis=1)
@@ -19,28 +22,26 @@ def MakeLinearRegressionModel(filename):
   correlation = dict(zip(list(X.columns), list(lrModel.coef_)))
   print(correlation)
 
-  plt.scatter(dataset.cost, lrModel.predict(X))
+  # plotting the predicted price vs actual price
+  plt.scatter(dataset.cost, lrModel.predict(X), marker='.')
   plt.xlabel("Prices")
   plt.ylabel("Predicted Prices")
   plt.title("Actual prices vs predicted prices")
   plt.show()
 
-def GetAirportScores(filename):
-  dataset = pd.read_csv(filename)
-  airportScores = {}
-  with open('airport_scores.csv', 'w') as airport_scores:
-    writer = csv.writer(airport_scores)
-    for example in dataset.iterrows():
-      info = example[1]
-      score = info['depart_pop'] + info['depart_ap_size'] + info['dep_ann_pass'] + info['dep_sunny']
-      airportScores[info['depart_ap']] = score
-    for key, val in airport_scores.items():
-      writer.writerow([key, val])
+  mseFull = np.mean((dataset.cost - lrModel.predict(X)) ** 2)
+  print(mseFull)
 
-    
+  x_train, x_test, y_train, y_test = train_test_split(
+    X, dataset.cost, test_size=0.33, random_state=5)
+  
+  lmTest = LinearRegression()
+  lmTest.fit(x_train, y_train)
 
-
+  mseTraindata = np.mean((y_train - lmTest.predict(x_train)) ** 2)
+  mseTestdata = np.mean((y_test - lmTest.predict(x_test)) ** 2)
+  print("train: " + str(mseTraindata))
+  print("test: " + str(mseTestdata))
 
 if __name__ == "__main__":
-  MakeLinearRegressionModel('flights_fix_scores.csv')
-  # GetAirportScores('flights_fix2.csv')
+  MakeLinearRegressionModel('flights_fix_scores_v2.csv')
